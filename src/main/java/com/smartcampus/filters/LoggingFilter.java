05c6108c20c85d2ff7ff4b924477a307de7986e3
tk <tskumarathunga@gmail.com>
2026-03-23 21:34:21 +0530

    Logging filter

---
+++ src/main/java/com/smartcampus/filters/LoggingFilter.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.filters;
+
+/**
+ *
+ * @author tk
+ */
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info("Incoming Request -> Method: " + requestContext.getMethod()
+                + ", URI: " + requestContext.getUriInfo().getAbsolutePath());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("Outgoing Response -> Status: " + responseContext.getStatus());
+    }
+}
